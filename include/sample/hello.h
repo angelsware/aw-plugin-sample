27e8dcdc3387b9ba6dbdc8bb9d259878d8c20279
Joachim Klahr <joachim.klahr@gmail.com>
2019-04-04 18:07:40 +0200

    First commit.

---
+++ include/sample/hello.h
@@ -0,0 +1,10 @@
+#ifndef __SAMPLE_HELLO_H__
+#define __SAMPLE_HELLO_H__
+
+class Hello
+{
+public:
+	void world();
+};
+
+#endif
---
+++ src/common/sample.cpp
@@ -0,0 +1,5 @@
+#include <log/aw_log.h>
+
+void Hello::world() {
+	LOG_INFO("Hello World!");
+}
